ae5d6e114286d1a0cf72b56e119445b52308810b
Sebastian <tengotreecreepers@gmail.com>
2020-03-10 21:48:54 -0700

    Added county shapefiles, added county population merging and some cleaning, updated gitignore for vscode development

--- transit_main.py
+++ transit_main.py
@@ -1,5 +1,34 @@
+import geopandas as gpd
+import pandas as pd
+
+COUNTY_SHP = 'data/cb_2018_53_cousub_500k/cb_2018_53_cousub_500k.shp'
+COUNTY_POP = 'data/cc-est2018-alldata-53.csv'
+
+
+def load_county_pop():
+    """
+    Loads the county populations file and merges it with county shape data.
+    """
+    shapes = gpd.read_file(COUNTY_SHP)
+    pop = pd.read_csv(COUNTY_POP)
+
+    # turn year column into "real" years
+    pop.loc[pop['YEAR'] < 3, 'YEAR'] = 3
+    pop['YEAR'] += 2007
+
+    # dissolve to counties
+    shapes['COUNTYFP'] = shapes['COUNTYFP'].astype(int)
+    shapes = shapes.dissolve(by='COUNTYFP')
+
+    joined = pop.merge(right=shapes, how='left',
+                       left_on='COUNTY', right_on='COUNTYFP')
+    return joined
+
+
 def main():
-	pass
+    county_pops = load_county_pop()
+    print(county_pops)
+
 
 if __name__ == '__main__':
-	main()
+    main()
