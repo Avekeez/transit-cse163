1460d136f7069d1f95b44f0d39c12f8659fef780
Sebastian <tengotreecreepers@gmail.com>
2020-03-14 16:02:26 -0700

    potential final code commit

--- transit_main.py
+++ transit_main.py
@@ -1,27 +1,18 @@
 import geopandas as gpd
 import pandas as pd
 import os
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point
 import matplotlib.pyplot as plt
-
-COUNTY_SHP = 'data/cb_2018_53_cousub_500k/cb_2018_53_cousub_500k.shp'
-COUNTY_POP = 'data/cc-est2018-alldata-53.csv'
+import seaborn as sns
 
 TRACT_SHP = 'data/tl_2010_53_tract00/tl_2010_53_tract00.shp'
 
 
-def read_shapes(shapes_txt):
-    df = pd.read_csv(shapes_txt)
-    df['point'] = [Point(-long, lat) for long, lat in
-                   zip(df['shape_pt_lon'], df['shape_pt_lat'])]
-    shapes = df.groupby('shape_id')['point'].apply(
-        lambda points: Polygon([[p.x, p.y] for p in points]))
-    df = df.drop_duplicates('shape_id')
-    df['geometry'] = shapes.values
-    return gpd.GeoDataFrame(df[['shape_id', 'geometry']])
-
-
 def load_bus_data():
+    """
+    Reads from all gtfs data for metro+sound in 2013 and 2017, merging together
+    geographical stop data and stop times.
+    """
     gtfs = 'data/gtfs/'
     dirs = os.listdir(gtfs)
     complete = None
@@ -33,15 +24,17 @@ def load_bus_data():
         dir = gtfs + dir + "/"
 
         stop_times = pd.read_csv(dir + "stop_times.txt")
-        stop_times = stop_times[['trip_id', 'arrival_time', 'departure_time', 'stop_id']]
+        stop_times = stop_times[['trip_id', 'arrival_time',
+                                 'departure_time', 'stop_id']]
 
         stops = pd.read_csv(dir + "stops.txt")
         stops['coordinate'] = [Point(long, lat) for long, lat in
-                   zip(stops['stop_lon'], stops['stop_lat'])]
-        stops = stops[['stop_id', 'stop_name', 'stop_desc', 'zone_id', 'coordinate']]
-        # stops = gpd.GeoDataFrame(stops, geometry='coordinate')
+                               zip(stops['stop_lon'], stops['stop_lat'])]
+        stops = stops[['stop_id', 'stop_name', 'stop_desc',
+                       'zone_id', 'coordinate']]
 
-        merged_stops = stops.merge(right=stop_times, how='right', left_on='stop_id', right_on='stop_id')
+        merged_stops = stops.merge(right=stop_times, how='right',
+                                   left_on='stop_id', right_on='stop_id')
         merged_stops['year'] = year
         merged_stops['system'] = syst
 
@@ -52,58 +45,92 @@ def load_bus_data():
     return gpd.GeoDataFrame(complete, geometry='coordinate')
 
 
-def load_county_pop():
-    """
-    Loads the county populations file and merges it with county shape data.
-    """
-    shapes = gpd.read_file(COUNTY_SHP)
-    pop = pd.read_csv(COUNTY_POP)
-
-    # turn year column into "real" years
-    pop.loc[pop['YEAR'] < 3, 'YEAR'] = 3
-    pop['YEAR'] += 2007
-
-    # dissolve to counties
-    shapes['COUNTYFP'] = shapes['COUNTYFP'].astype(int)
-    shapes = shapes.dissolve(by='COUNTYFP')
-
-    joined = pop.merge(right=shapes, how='left',
-                       left_on='COUNTY', right_on='COUNTYFP')
-    return joined
-
-
 def load_tract_incomes():
+    """
+    Combines data for household incomes per tract in multiple counties,
+    merging them with shape files for the tracts.
+    """
     shapes = gpd.read_file(TRACT_SHP)
     inc_dirs = 'data/incomes/'
     dirs = os.listdir(inc_dirs)
     complete = None
     for dir in dirs:
         income = pd.read_csv(inc_dirs + dir)
-        income['ID Geography'] = income['ID Geography'].apply(lambda tract: int(tract.split('US')[1]))
+        income['ID Geography'] = income['ID Geography'].apply(
+            lambda tract: int(tract.split('US')[1]))
         if complete is None:
             complete = income
         else:
             complete = complete.append(income)
-    complete = complete.merge(right=shapes, how='left', left_on='ID Geography', right_on='CTIDFP00')
+    complete = complete.merge(right=shapes, how='left',
+                              left_on='ID Geography', right_on='CTIDFP00')
     return gpd.GeoDataFrame(complete)
 
 
 def plot_income_change_over_availability(incomes, stops):
-    # stops.plot()
+    """
+    Creates a scatter plot of all census tracts, plotting their average incomes
+    against their bus availability.
+    """
     incomes_2017 = incomes[incomes['Year'] == 2017].dropna(subset=['geometry'])
+    incomes_2013 = incomes[incomes['Year'] == 2013].dropna(subset=['geometry'])
     stops_2017 = stops[(stops['year'] == 2017)].dropna(subset=['coordinate'])
-    print(stops_2017)
-    covered_tracts = gpd.sjoin(incomes_2017, stops_2017, how='inner', op='intersects')
-    covered_tracts.groupby('Geography').size()  # for each tract, get number of stops
-    # todo...
+
+    covered_tracts = gpd.sjoin(incomes_2017, stops_2017,
+                               how='inner', op='intersects')
+    stops_per_tract = covered_tracts.groupby('ID Geography').size()
+
+    incomes_2017 = incomes_2017[['Household Income by Race', 'ID Geography']]
+    incomes_2017['income_2017'] = incomes_2017['Household Income by Race']
+    incomes_2013 = incomes_2013[['Household Income by Race', 'ID Geography']]
+    incomes_2013['income_2013'] = incomes_2013['Household Income by Race']
+
+    d_incomes = incomes_2013.merge(right=incomes_2017,
+                                   how='inner', on='ID Geography')
+    d_incomes['delta'] = d_incomes['income_2017'] - d_incomes['income_2013']
+
+    d_incomes = d_incomes.merge(right=stops_per_tract.rename('stops'),
+                                how='inner', on='ID Geography')
+    sns.regplot(data=d_incomes, x='stops', y='delta')
+
+    plt.xlabel('Total # of stops in the Tract')
+    plt.ylabel('Change in average household income in the tract')
+    plt.ylim(-21000, 60000)
+    plt.subplots_adjust(left=0.2)
+    plt.title('Income Change over Stop Count')
+    plt.savefig('income_change_over_availability.png')
+
+
+def plot_buses_vs_income(bus_data, income_data):
+    """
+    Takes in the bus stop data and income data and creates a map between years
+    """
+    fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2, figsize=(20, 10), ncols=2)
+    income_data[income_data['Year'] == 2013].plot(
+        ax=ax1, column='Household Income by Race', legend=True)
+    bus_data[bus_data['year'] == 2013].plot(
+        ax=ax1, marker='*', color='#000000', markersize=.001, alpha=0.5)
+    income_data[income_data['Year'] == 2013].plot(
+        ax=ax3,  column='Household Income by Race', legend=True)
+
+    income_data[income_data['Year'] == 2017].plot(
+        ax=ax2, column='Household Income by Race', legend=True)
+    bus_data[bus_data['year'] == 2017].plot(
+        ax=ax2, marker='*', color='#000000', markersize=.001, alpha=0.5)
+    income_data[income_data['Year'] == 2017].plot(
+        ax=ax4,  column='Household Income by Race', legend=True)
+    ax1.set_title('2013 income, with bus stops')
+    ax3.set_title('2013 income, without bus stops')
+    ax2.set_title('2017 income, with bus stops')
+    ax4.set_title('2017 income, without bus stops')
+    fig.savefig("bus_vs_income.png")
 
 
 def main():
-    # county_pops = load_county_pop()
-    # print(county_pops)
     stops = load_bus_data()
     incomes = load_tract_incomes()
     plot_income_change_over_availability(incomes, stops)
+    # plot_buses_vs_income(stops, incomes)
 
 
 if __name__ == '__main__':
